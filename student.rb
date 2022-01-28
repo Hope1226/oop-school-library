b7de84d19ff756d23f4bda3a56135ccefa579401
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-28 21:36:21 +0500

    Introduce a bug

--- student.rb
+++ student.rb
@@ -3,6 +3,10 @@ require_relative './person'
 class Student < Person
   attr_accessor :classroom
 
+  def initialize(age, name, parent_permission: true)
+    super(age, name, parent_permission)
+  end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
