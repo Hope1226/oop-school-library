8d836b1949ef2d9d2ebf19614c74ce4fbb0c18f5
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-28 23:50:04 +0500

    Introduce new bug

--- main.rb
+++ main.rb
@@ -76,7 +76,7 @@ class Main
       puts '( No Books Found )'.red
     else
       @book_list.each_with_index.map do |book, index|
-        puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}".yellow
+        puts "1) Title: \"#{book.title}\", Author: #{book.author}".yellow
       end
     end
 
--- student.rb
+++ student.rb
@@ -3,10 +3,6 @@ require_relative './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, name, parent_permission: true)
-    super(age, name, parent_permission)
-  end
-
   def play_hooky
     '¯\(ツ)/¯'
   end
