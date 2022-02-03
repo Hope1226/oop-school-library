4cae3e5a4c96cb724ffb2204f2ed8d8fa374b0e5
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-02-03 22:42:30 +0500

    Add unit tests for book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,15 @@
+require '../book'
+
+describe Book do
+  context 'When testing Book class' do
+    it 'the book title should return the correct book title' do
+      temp_book = Book.new('Rich Dad and Poor Dad', 'Robert Kiosaki')
+      expect(temp_book.title).to eql 'Rich Dad and Poor Dad'
+    end
+
+    it 'the book reantals length should return 0' do
+      temp_book = Book.new('Rich Dad and Poor Dad', 'Robert Kiosaki')
+      expect(temp_book.rentals.length).to eql 0
+    end
+  end
+end
