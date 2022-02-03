53ec4bf0f3f13bf7eaa480a2e2cd4675f0fb6b50
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-02-03 23:12:54 +0500

    Add unit tests for student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,15 @@
+require '../student'
+
+describe Student do
+  context 'When testin the student class' do
+    it 'the students name should be correct' do
+      student_test = Student.new(23, 'Jonny')
+      expect(student_test.name).to eql 'Jonny'
+    end
+
+    it 'the student class should be instance of Person class' do
+      student_test = Student.new(23, 'Jonny')
+      expect(student_test).to be_kind_of(Person)
+    end
+  end
+end
