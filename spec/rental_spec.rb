f48eccd04fa4274f93cbedb2f2c2b50e9c403e3c
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-03 21:07:37 +0200

    Solve linter errors

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -3,10 +3,8 @@ require './rental'
 describe Rental do
   context 'when testing Rental' do
     it 'creates a new instance of Rental' do
-      rental1 = Rental.new('12.01.2022')
-      expect(rental1.date).to eq('12.01.2022')
-      # expect(rental1.book).to eq 'Wild Russia'
-      # expect(rental1.person).to eq 'Maximilianovici'
+      rental1 = Rental.new('21/2/2022')
+      expect(rental1.date).to eq('21/2/2022')
     end
   end
 end
